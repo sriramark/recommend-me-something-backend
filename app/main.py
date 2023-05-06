170e555a7b568776c6eea3eda2e5e6220f7b4f1b
sriramark <sriramarkk1@gmail.com>
2023-05-06 17:36:59 +0530

    CORS listed as wildcard

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import books
 
+origins = ['*']
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_methods=['GET']
+)
 
 app.include_router(books.router)
 
